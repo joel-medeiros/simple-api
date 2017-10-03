59ced3e8bb884cf6465f00cba0a8384874ca7df2
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-10-03 19:01:43 -0300

    Basics for api and tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,4 +22,10 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         return $app;
     }
+
+    public function getAuth()
+    {
+        $user = \App\User::find(1);
+        return ['Authorization' => 'Bearer ' . $user->api_token];
+    }
 }
