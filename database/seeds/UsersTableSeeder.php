1dc0928029ad25f0c173c942a940202606894f4c
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-10-05 01:20:59 -0300

    set default user to api

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class)->create();
+        DB::table('users')->insert([
+            'name' => 'Joel Medeiros',
+            'email' => 'jooel.medeiros@gmail.com',
+            'password' => bcrypt('abcd=1234'),
+            'remember_token' => str_random(10),
+            'api_token' => 'm4Ut0k3NL3g4Ln40',
+        ]);
     }
 }
